b167b3523c205c9d09988dc374d96d36113f9f4b
Sebastian Nocke <sjno@gmx.de>
2023-04-07 01:24:48 +0200

    Improvements on calculate_mesh function

--- src/terrain/mesh.rs
+++ src/terrain/mesh.rs
@@ -26,62 +26,92 @@ pub fn regenerate_mesh(terrain: &Terrain, meshes: &mut ResMut<Assets<Mesh>>, ima
 }
 
 fn caluclate_mesh(terrain: &Terrain, images: &Res<Assets<Image>>) -> Mesh {
-    
-    let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
+    // extract some values from the parameters for better readabillity
     let total_width = terrain.size.x;
     let total_length = terrain.size.z;
     let half_width = total_width / 2.0;
     let half_length = total_length / 2.0;
     let max_height = terrain.size.y;
 
+    // retreive the heightmap image and extract its size
     let heightmap = images.get(&terrain.heightmap).unwrap();
     let width_resolution = heightmap.size().x as usize;
-    let lenght_resolution = heightmap.size().y as usize;
+    let length_resolution = heightmap.size().y as usize;
     
-    let mut positions = Vec::with_capacity(width_resolution * lenght_resolution);
-    let mut shade_colors = Vec::with_capacity(width_resolution * lenght_resolution);
+    // calculate the ammount of vertices for the mesh
+    let vertices_count = width_resolution * length_resolution;
 
-    for y in 0..lenght_resolution {
+    // initialize the positions and shade_colors vectors with
+    // capacity of verticies_count which leads to no additional
+    // allocations during mesh construction
+    let mut positions = Vec::with_capacity(vertices_count);
+    let mut shade_colors = Vec::with_capacity(vertices_count);
+
+    for y in 0..length_resolution {
         for x in 0..width_resolution {
-            // currently only for 4 channel
-            let point_index = y * width_resolution + x;
-            let raw_height = heightmap.data[point_index * 4];
+            // calculate the index of the pixel for the linear data vector
+            let pixel_index = y * width_resolution + x;
+            // retreive the raw height data
+            // currently only images using 4 channels per pixel
+            // are supported but only the red channel is used
+            // this leads to the * 4 multiplication of the pixel_index
+            let raw_height = heightmap.data[pixel_index * 4];
+            // the raw_height is normalized between 0 and 1 as
+            // its maximal possible value is 255
             let normalized_height = raw_height as f32 / 255.0;
+            // calculate the actual height by multiplying with max_height
             let height = normalized_height * max_height;
             
+            // calculate 'percentage of loop variable in their resolution
+            // before scaling with the total size and offseting them by half
             let width_position = (x as f32 / width_resolution as f32) * total_width - half_width;
-            let length_position = (y as f32 / lenght_resolution as f32) * total_length - half_length;
+            let length_position = (y as f32 / length_resolution as f32) * total_length - half_length;
 
+            // push the calculated position into the positions vector
             positions.push([width_position, height, length_position]);
 
+            // approximate a hue gradient traversing the terrain diagonally
             let lower_hue = (x as f32 / width_resolution as f32) * 18.0 + 1.0;
-            let upper_hue = (y as f32 / lenght_resolution as f32) * 18.0 + 1.0;
+            let upper_hue = (y as f32 / length_resolution as f32) * 18.0 + 1.0;
             let total_hue = lower_hue * upper_hue;
             let color = Color::Hsla { hue: total_hue, saturation: 0.8, lightness: 0.3, alpha: 1.0 };
             
+            // push the calculated color into the shade_colors vector
             shade_colors.push(color.as_rgba_f32());
         }
     }
 
-    let mut line_indices = Vec::with_capacity(positions.len() * 2);
+    // calculate the count of 'rectangles' and triangles that the terrain consists of
+    let rectangle_count = (length_resolution - 1) * (width_resolution - 1);
+    let triangle_count = rectangle_count * 2;
+    
+    // initialize indicies with an initial capacity of triangle_count * 3
+    // as each triangle consists of three points
+    let mut indices = Vec::with_capacity(triangle_count * 3);
 
-    for l in 0..(lenght_resolution as u32) - 1 {
+    for l in 0..(length_resolution as u32) - 1 {
         for w in 0..(width_resolution as u32) - 1 {
+            // the first two indices are in the 'current' line
             let first = l * (width_resolution as u32) + w;
             let second = first + 1;
+            // the last two indices are in the 'next' line
             let thrid = (l + 1) * (width_resolution as u32) + w;
             let fourth = thrid + 1;
 
-            line_indices.push(first);
-            line_indices.push(thrid);
-            line_indices.push(second);
+            // first triangle
+            indices.push(first);
+            indices.push(thrid);
+            indices.push(second);
             
-            line_indices.push(thrid);
-            line_indices.push(fourth);
-            line_indices.push(second);
+            // second triangle
+            indices.push(thrid);
+            indices.push(fourth);
+            indices.push(second);
         }
     }
-
+    
+    let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
+    
     mesh.insert_attribute(
         Mesh::ATTRIBUTE_POSITION, 
         positions,
@@ -94,62 +124,16 @@ fn caluclate_mesh(terrain: &Terrain, images: &Res<Assets<Image>>) -> Mesh {
 
     mesh.insert_attribute(
         Mesh::ATTRIBUTE_NORMAL,
-        vec![[0.0, 1.0, 0.0]; width_resolution * lenght_resolution],
+        vec![[0.0, 1.0, 0.0]; width_resolution * length_resolution],
     );
 
     mesh.insert_attribute(
         Mesh::ATTRIBUTE_UV_0,
-        vec![[0.0, 0.0]; width_resolution * lenght_resolution],
+        vec![[0.0, 0.0]; width_resolution * length_resolution],
     );
 
     mesh.set_indices(Some(mesh::Indices::U32(
-        line_indices
-    )));
-    
-    mesh
-}
-
-#[allow(dead_code)]
-fn caluclate_flat_mesh(terrain: &Terrain) -> Mesh {
-    
-    let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
-    let total_width = terrain.size.x;
-    let total_length = terrain.size.z;
-    let half_width = total_width / 2.0;
-    let half_length = total_length / 2.0;
-
-    mesh.insert_attribute(
-        Mesh::ATTRIBUTE_POSITION, 
-        vec![
-            [-half_width, 0.0, -half_length],
-            [ half_width, 0.0, -half_length],
-            [-half_width, 0.0,  half_length],
-            [ half_width, 0.0,  half_length],
-        ],
-    );
-    
-    mesh.insert_attribute(
-        ATTRIBUTE_SHADE_COLOR, 
-        vec![
-            Color::rgb(1.0, 0.0, 0.0).as_rgba_f32(),
-            Color::rgb(0.0, 1.0, 0.0).as_rgba_f32(),
-            Color::rgb(0.0, 0.0, 1.0).as_rgba_f32(),
-            Color::rgb(0.0, 0.0, 0.0).as_rgba_f32(),
-        ],
-    );
-
-    mesh.insert_attribute(
-        Mesh::ATTRIBUTE_NORMAL,
-        vec![[0.0, 1.0, 0.0]; 4],
-    );
-
-    mesh.insert_attribute(
-        Mesh::ATTRIBUTE_UV_0,
-        vec![[0.0, 0.0]; 4],
-    );
-    
-    mesh.set_indices(Some(mesh::Indices::U32(
-        vec![0, 2, 1, 2, 3, 1]
+        indices
     )));
     
     mesh
